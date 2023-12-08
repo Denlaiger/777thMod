fb29582c980d1307c3f4fa5e28e23b24e1ac4275
Den <153307632+Denlaiger@users.noreply.github.com>
2023-12-08 15:58:52 -0500

    Update config.cpp

--- @777thMod/addons/777thArmour/config.cpp
+++ @777thMod/addons/777thArmour/config.cpp
@@ -3,8 +3,7 @@ class CfgPatches {
 		author = "Den, Villainess";
 		name = "777th Armor - Misriah Armours";
 		requiredAddons[] = {
-			"MA_Armor",
-			"Tenthed_Armor_Misriah_Base"
+			"MA_Armor"
 		};
 		weapons[] = {};
 		units[] = {};
@@ -26,146 +25,15 @@ class cfgWeapons {
 	};
 	class ItemInfo;
 
-	class Tenthed_M56SR_Helmet;
-	class Tenthed_M56A_Helmet;
-	class Tenthed_M56G_Helmet;
-	class Tenthed_M56E_Helmet;
-	class Tenthed_M56_Scout_Helmet;
+	class TripSeventh_M56SR_Helmet;
+	class TripSeventh_M56A_Helmet;
+	class TripSeventh_M56G_Helmet;
+	class TripSeventh_M56E_Helmet;
+	class TripSeventh_M56_Scout_Helmet;
 
-	class Tenthed_Vest_Base;
+	class TripSeventh_Vest_Base;
 
-	// Akira
-	#include "data\Akira\armor.cpp"
-
-	// Tiny
-	#include "data\Tiny\armor.cpp"
-
-	// Lion
-	#include "data\Lion\armor.cpp"
-
-	// Nova
-	#include "data\Nova\armor.cpp"
-
-	// Villainess
-	#include "data\Villainess\armor.cpp"
 	
-	// Void
-	#include "data\Void\armor.cpp"
+	#include "data\armor.cpp"
 
-	// Mac
-	#include "data\Mac\armor.cpp"
-	
-	// Bbstine
-	#include "data\Bbstine\armor.cpp"
-
-	// Flipper
-	#include "data\Flipper\armor.cpp"
-
-	// Rambler
-	#include "data\Rambler\armor.cpp"
-
-	// Slim
-	#include "data\Slim\armor.cpp"
-
-	// Boomstick
-	#include "data\Boomstick\armor.cpp"
-    
-    // Blindwolf
-	#include "data\Blindwolf\armor.cpp"
-
-	// Jad
-	#include "data\Jad\armor.cpp"
-//
-	// Clem
-	#include "data\Clem\armor.cpp"
-
-	// Cookie
-	#include "data\Cookie\armor.cpp"
-
-	// Dovah
-	#include "data\Dovah\armor.cpp"
-
-	// Drake
-	#include "data\Drake\armor.cpp"
-
-	// Gutts
-	#include "data\Gutts\armor.cpp"
-
-	// HardDrop
-	#include "data\HardDrop\armor.cpp"
-
-	// Hatchet
-	#include "data\Hatchet\armor.cpp"
-
-	// Leek
-	#include "data\Leek\armor.cpp"
-
-	// Nobody
-	#include "data\Nobody\armor.cpp"
-
-	// Seagull
-	#include "data\Seagull\armor.cpp"
-
-	// Som
-	#include "data\Som\armor.cpp"
-//
-	// Cossack
-	#include "data\Cossack\armor.cpp"
-
-	// Evader
-	#include "data\Evader\armor.cpp"
-
-	// Freedom
-	#include "data\Freedom\armor.cpp"
-
-	// Goldy
-	#include "data\Goldy\armor.cpp"
-
-	// Grimgnaw
-	#include "data\Grimgnaw\armor.cpp"
-
-	// Hodge
-	#include "data\Hodge\armor.cpp"
-
-	// Infra
-	#include "data\Infra\armor.cpp"
-
-	// McInnes
-	#include "data\McInnes\armor.cpp"
-
-	// Meatshield
-	#include "data\Meatshield\armor.cpp"
-
-	// Tiger
-	#include "data\Tiger\armor.cpp"
-//
-	// Atlas
-	#include "data\Atlas\armor.cpp"
-
-	// Dex
-	#include "data\Dex\armor.cpp"
-
-	// Dubbos
-	#include "data\Dubbos\armor.cpp"
-
-	// Fwog
-	#include "data\Fwog\armor.cpp"
-
-	// Glint
-	#include "data\Glint\armor.cpp"
-
-	// Jody
-	#include "data\Jody\armor.cpp"
-
-	// Marvel
-	#include "data\Marvel\armor.cpp"
-
-	// Mom
-	#include "data\Mom\armor.cpp"
-
-	// Optional
-	#include "data\Optional\armor.cpp"
-
-	// Storm
-	#include "data\Storm\armor.cpp"
 };
