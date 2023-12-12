f2760d150b11236c44f2914f3ce60eb70af1d700
Den <153307632+Denlaiger@users.noreply.github.com>
2023-12-12 17:02:57 -0500

    Update config.cpp

--- @777thMod/addons/Fortify_Settings/config.cpp
+++ @777thMod/addons/Fortify_Settings/config.cpp
@@ -15,9 +15,7 @@ class ACEX_Fortify_Presets {
         displayName = "777th PMC - Fortify";
         objects[] = {
             {"Land_Plank_01_4m_F", 5},
-            {"Land_OPTRE_M72S_barrier", 13}, // possible removal
-            {"Land_OPTRE_M72_barrier", 13}, // possible removal
-            {"Land_HBarrier_1_F", 15}, // possible removal
+            {"Land_HBarrier_1_F", 10}, 
             {"Land_HBarrierWall4_F", 15},
             {"Land_HBarrierWall6_F", 15},
             {"Land_HBarrierWall_corner_F", 15},
